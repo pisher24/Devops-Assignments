4cfed3aed4bce5e17f69c3446d0e5975d8922210
pisher24 <117724184+pisher24@users.noreply.github.com>
2025-09-02 18:32:50 +0530

    read data from previous write file

---
+++ Assignment-2/task-4.py
@@ -0,0 +1,12 @@
+# Open the file in read mode
+file = open("myfile.txt", "r")
+
+# Read the entire content
+content = file.read()
+
+# Print the content
+print("File content:")
+print(content)
+
+# Close the file
+file.close()
