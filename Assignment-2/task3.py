85fe9f713184cb5a9578d5e8b7e5b841afa98e10
pisher24 <117724184+pisher24@users.noreply.github.com>
2025-09-02 18:27:58 +0530

    Open and write data to a file

---
+++ Assignment-2/task3.py
@@ -0,0 +1,15 @@
+# Open a file in write mode ("w")
+# If the file does not exist, it will be created
+# If it already exists, its content will be overwritten
+
+file = open("myfile.txt", "w")
+
+# Write content to the file
+file.write("Hello, this is my first text file!\n")
+file.write("Python makes file handling easy.\n")
+file.write("Good luck learning file operations!")
+
+# Always close the file after writing
+file.close()
+
+print("File 'myfile.txt' created and content written successfully!")
